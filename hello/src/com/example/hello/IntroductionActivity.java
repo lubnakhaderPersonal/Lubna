1d484384e5fcb7663a3be4a7d2619f89ac1e1d01
sreekumarrajan <sreekumar.rajan@mail.utoronto.ca>
2014-04-01 00:59:42 -0400

    introduction slides proper

--- hello/src/com/example/hello/IntroductionActivity.java
+++ hello/src/com/example/hello/IntroductionActivity.java
@@ -10,15 +10,35 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
+import android.widget.ViewSwitcher.ViewFactory;
 
 public class IntroductionActivity extends Activity implements OnClickListener{
-private VideoView video;
-private MediaController ctlr;
+
+private ImageButton img;
+private ImageSwitcher imageSwitcher;
 private Button nextButton;
+private int slideCount = 0;
+int imgs[] = 
+{ 
+	R.drawable.slide1, 
+	R.drawable.slide2, 
+	R.drawable.slide3, 
+	R.drawable.slide4,
+	R.drawable.slide5,
+	R.drawable.slide6,
+	R.drawable.slide7,
+	
+};
 @Override
 public void onCreate(Bundle icicle) {
 super.onCreate(icicle);
@@ -26,22 +46,23 @@ getWindow().setFormat(PixelFormat.TRANSLUCENT);
 setContentView(R.layout.introduction_layout);
 nextButton = (Button)findViewById(R.id.nextbutton);
 nextButton.setOnClickListener(this);
-//File clip=new File(Environment.getExternalStorageDirectory(),
-//"test.mp4");
-String videoName = "introvideo";
-int id = getResources().getIdentifier(videoName, "raw", getPackageName());
-final String path = "android.resource://" + getPackageName() + "/" + id;
 
-//vvBgVideo.setVideoURI(Uri.parse(path));
-video=(VideoView)findViewById(R.id.video);
-//video.setVideoPath(clip.getAbsolutePath());
-video.setVideoURI(Uri.parse(path));
-ctlr=new MediaController(this);
-ctlr.setMediaPlayer(video);
-video.setMediaController(ctlr);
-video.start();
-video.requestFocus();
+img = (ImageButton)findViewById(R.id.imageButton1);
+imageSwitcher = (ImageSwitcher)findViewById(R.id.imageSwitcher1);
 
+imageSwitcher.setFactory(new ViewFactory() {
+
+@Override
+public View makeView() {
+ImageView myView = new ImageView(getApplicationContext());
+myView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+myView.setLayoutParams(new ImageSwitcher.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT));
+		return myView;
+ }
+
+});
+
+imageSwitcher.setImageResource(imgs[slideCount]);
 }
 @Override
 public void onClick(View view) {
@@ -49,12 +70,32 @@ public void onClick(View view) {
 	switch(view.getId())
 	{
 	case R.id.nextbutton:
-
-		//Toast.makeText(this,"allfine",Toast.LENGTH_LONG).show();
-	    //video.clearFocus();
 		Intent i = new Intent(this,MainActivity.class);
 		startActivity(i);
 		break;
 	}
 }
+
+public void next(View view){
+    slideCount = (slideCount+1)%7;
+    Animation in = AnimationUtils.loadAnimation(this,
+    android.R.anim.slide_in_left);
+    Animation out = AnimationUtils.loadAnimation(this,
+    android.R.anim.slide_out_right);
+    imageSwitcher.setInAnimation(in);
+    imageSwitcher.setOutAnimation(out);
+    imageSwitcher.setImageResource(imgs[slideCount]);
+ }
+ public void previous(View view){
+    slideCount = (slideCount-1);
+    if(slideCount < 0)
+    	slideCount = 6;
+    Animation in = AnimationUtils.loadAnimation(this,
+    android.R.anim.slide_out_right);
+    Animation out = AnimationUtils.loadAnimation(this,
+    android.R.anim.slide_in_left);
+    imageSwitcher.setInAnimation(out);
+    imageSwitcher.setOutAnimation(in);
+    imageSwitcher.setImageResource(imgs[slideCount]);
+ }
 }
